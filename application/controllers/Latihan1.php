f2ab085d02d46f1e1632410072a277a57a4fc92c
inayaokta <inayaoktavianadh@gmail.com>
2023-10-03 20:50:11 +0700

    push pustaka-booking1

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -6,6 +6,7 @@ class Latihan1 extends CI_Controller
         echo "Selamat Datang.. Selamat belajar Web Programming";
         //$this->load->view('view-latihan1');
     }
+    
     public function penjumlahan($n1, $n2)
     {
         $this->load->model('Model_latihan1');
